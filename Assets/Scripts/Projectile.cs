2aeb332f0395125fa7ea58e5ee122185980b98a8
Bülent T. Yüksel <bulenttyuksel@gmail.com>
2022-12-27 21:39:34 +0300

    damage text eklendi

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public int baseDamage = 10;
+    public int criticalDamage = 20;
     public int damage;
     public float baseExplosionRadius = 2.0f;
     public float explosionRadius;
@@ -49,7 +50,6 @@ public class Projectile : MonoBehaviour
 
         foreach (Collider2D col in objectsInRange)
         {
-            Debug.Log(col.tag);
             if (col.tag == "Player")
             {
                 var healthComponent = col.GetComponent<Health>();
@@ -58,8 +58,19 @@ public class Projectile : MonoBehaviour
                 {
                     float proximity = (center - new Vector2(healthComponent.transform.position.x, healthComponent.transform.position.y)).magnitude;
                     float effect = 1 - (proximity / radius);
+                    bool isCriticalHit = false;
 
-                    healthComponent.TakeDamage((int)(damage * effect));
+                    if (effect > 0.7f)
+                    {
+                        isCriticalHit = Random.Range(0, 10) < 3;
+                        damage = criticalDamage;
+                    }
+                    else
+                    {
+                        damage = baseDamage;
+                    }
+
+                    healthComponent.TakeDamage((int)(damage * effect), isCriticalHit);
                 }
             }
 
