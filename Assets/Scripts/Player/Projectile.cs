de3cfa5eefc8004b526dc25c16e67f537aedad8b
Bülent T. Yüksel <bulenttyuksel@gmail.com>
2023-04-25 23:42:18 +0300

    feature implemented

--- Assets/Scripts/Player/Projectile.cs
+++ Assets/Scripts/Player/Projectile.cs
@@ -2,87 +2,134 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+namespace DTerrain
 {
-    public int baseDamage = 10;
-    public int criticalDamage = 20;
-    public float baseExplosionRadius = 2.0f;
-    public float explosionRadius;
-    // Start is called before the first frame update
-    void Start()
+
+    public class Projectile : MonoBehaviour
     {
-        explosionRadius = baseExplosionRadius;
-    }
+        public int baseDamage = 10;
+        public int criticalDamage = 20;
+        public int baseExplosionRadius = 2;
+        public int explosionRadius;
+        public int destructionRadius;
 
-    // Update is called once per frame
-    void Update()
-    {
+        protected Shape destroyCircle;
+        private BasicPaintableLayer primaryLayer;
+        private BasicPaintableLayer secondaryLayer;
 
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-
-        // Direkt çarpışmada damage veren kod
-        /* if (other.tag == "Player")
+        void Start()
         {
-            var healthComponent = other.GetComponent<Health>();
-            if (healthComponent != null)
+            GameObject destructor = GameObject.FindGameObjectWithTag("Destructor");
+            if (destructor)
             {
-                healthComponent.TakeDamage(damage);
-            }
-        } */
-
-
-    }
-
-    public bool getPlayerLuck()
-    {
-        // TODO ileride playerdan alırız
-        return Random.Range(0, 10) < 3;
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        InflictAreaDamage(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y), explosionRadius);
-        Destroy(gameObject);
-    }
-
-    void InflictAreaDamage(Vector2 center, float radius)
-    {
-        Collider2D[] objectsInRange = Physics2D.OverlapCircleAll(center, radius);
-
-        foreach (Collider2D col in objectsInRange)
-        {
-            if (col.tag == "Player")
-            {
-                var healthComponent = col.GetComponent<Health>();
-
-                if (healthComponent != null)
+                ClickAndDestroyOptimized destroyScript = destructor.GetComponent<ClickAndDestroyOptimized>();
+                if (destroyScript)
                 {
-                    float proximity = (center - new Vector2(healthComponent.transform.position.x, healthComponent.transform.position.y)).magnitude;
-                    float effect = 1 - (proximity / radius);
-                    bool isCriticalHit = false;
-
-                    int damage = baseDamage;
-
-                    if (effect >= 0.8f)
-                    {
-                        isCriticalHit = getPlayerLuck();
-
-                        if (isCriticalHit)
-                            damage = criticalDamage;
-                    }
-
-                    int damageInflicted = (int)(damage * effect);
-
-                    if (damageInflicted <= 0)
-                        continue;
-
-                    healthComponent.takeDamage(damageInflicted, isCriticalHit);
+                    primaryLayer = destroyScript.primaryLayer;
+                    secondaryLayer = destroyScript.secondaryLayer;
                 }
             }
 
+            explosionRadius = baseExplosionRadius;
+            destructionRadius = explosionRadius * 5;
+            destroyCircle = Shape.GenerateShapeCircle(destructionRadius);
+        }
+
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+
+            // Direkt çarpışmada damage veren kod
+            /* if (other.tag == "Player")
+            {
+                var healthComponent = other.GetComponent<Health>();
+                if (healthComponent != null)
+                {
+                    healthComponent.TakeDamage(damage);
+                }
+            } */
+
+
+        }
+
+        public bool getPlayerLuck()
+        {
+            // TODO ileride playerdan alırız
+            return Random.Range(0, 10) < 3;
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            DestroyTerrain(gameObject.transform.position, destructionRadius);
+            InflictAreaDamage(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y), explosionRadius);
+            Destroy(gameObject);
+        }
+
+        protected virtual void DestroyTerrain(Vector3 position, int r)
+        {
+            //position.z = -10;
+            Debug.Log($"projectile position on contact: {position}");
+            Vector3 p = position - primaryLayer.transform.position;
+
+            primaryLayer?.Paint(new PaintingParameters()
+            {
+                Color = Color.clear,
+                Position = new Vector2Int((int)(p.x * primaryLayer.PPU) - r, (int)(p.y * primaryLayer.PPU) - r),
+                Shape = destroyCircle,
+                PaintingMode = PaintingMode.REPLACE_COLOR,
+                DestructionMode = DestructionMode.DESTROY
+            });
+
+            secondaryLayer?.Paint(new PaintingParameters()
+            {
+                Color = Color.clear,
+                Position = new Vector2Int((int)(p.x * secondaryLayer.PPU) - r, (int)(p.y * secondaryLayer.PPU) - r),
+                Shape = destroyCircle,
+                PaintingMode = PaintingMode.REPLACE_COLOR,
+                DestructionMode = DestructionMode.NONE
+            });
+        }
+
+        void InflictAreaDamage(Vector2 center, float radius)
+        {
+            Collider2D[] objectsInRange = Physics2D.OverlapCircleAll(center, radius);
+
+            foreach (Collider2D col in objectsInRange)
+            {
+                if (col.CompareTag("Player"))
+                {
+                    var healthComponent = col.GetComponent<Health>();
+
+                    if (healthComponent != null)
+                    {
+                        float proximity = (center - new Vector2(healthComponent.transform.position.x, healthComponent.transform.position.y)).magnitude;
+                        float effect = 1 - (proximity / radius);
+                        bool isCriticalHit = false;
+
+                        int damage = baseDamage;
+
+                        if (effect >= 0.8f)
+                        {
+                            isCriticalHit = getPlayerLuck();
+
+                            if (isCriticalHit)
+                                damage = criticalDamage;
+                        }
+
+                        int damageInflicted = (int)(damage * effect);
+
+                        if (damageInflicted <= 0)
+                            continue;
+
+                        healthComponent.takeDamage(damageInflicted, isCriticalHit);
+                    }
+                }
+
+            }
         }
     }
 }
